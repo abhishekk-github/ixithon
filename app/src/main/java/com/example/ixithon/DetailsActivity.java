f88a775213d2a97f6e30509b2a20a68c1d798a2e
Abhishek Kumar <abhishek.kumar@aspiringminds.in>
2017-04-08 21:56:40 +0530

    Home page design with screens

---
+++ app/src/main/java/com/example/ixithon/DetailsActivity.java
@@ -0,0 +1,101 @@
+package com.example.ixithon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.example.ixithon.model.CityDescription;
+import com.example.ixithon.network.SingltonRequestQueue;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+public class DetailsActivity extends AppCompatActivity {
+
+  TextView name,country,Description;
+  Button plan;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_details);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    name = (TextView) findViewById(R.id.des_name);
+    country = (TextView) findViewById(R.id.des_country);
+    Description = (TextView) findViewById(R.id.des_description);
+    plan = (Button) findViewById(R.id.trip_plan_btn);
+    String s = getIntent().getStringExtra("CityID");
+    fetchDataFromServer(s);
+
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_abc);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show();
+      }
+    });
+
+    plan.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(DetailsActivity.this, TripDetailsActivity.class);
+       // intent.putExtra("CityID", items.getID());
+        startActivity(intent);
+      }
+    });
+  }
+
+  /**
+   * this will fetch the responce from the list of items to be displayed.
+   */
+  private void fetchDataFromServer(String cityid) {
+    String JSON_URL = "http://build2.ixigo.com/api/v3/namedentities/id/"+cityid+"?apiKey=ixicode!2$";
+    StringRequest stringRequest = new StringRequest(JSON_URL.replace(" ",""),
+        new Response.Listener<String>() {
+          @Override
+          public void onResponse(String response) {
+            Log.v("MSG",response);
+            CityDescription  cityDescription =  CityDescription.getCityDescriptionFromServer(response);
+            name.setText(cityDescription.getName());
+            country.setText(cityDescription.getCountryName());
+            Description.setText(cityDescription.getDescription());
+          }
+        },
+        new Response.ErrorListener() {
+          @Override
+          public void onErrorResponse(VolleyError error) {
+            NetworkResponse response = error.networkResponse;
+            if (error instanceof ServerError && response != null) {
+              try {
+                String res = new String(response.data,
+                    HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+                JSONObject obj = new JSONObject(res);
+              } catch (UnsupportedEncodingException | JSONException e1) {
+                // Couldn't properly decode data to string
+                e1.printStackTrace();
+              }
+            }
+          }
+        });
+    RequestQueue requestQueue = SingltonRequestQueue.getInstance(this).getRequestQueue();
+    requestQueue.add(stringRequest);
+  }
+}
---
+++ app/src/main/java/com/example/ixithon/model/CityDescription.java
@@ -0,0 +1,124 @@
+package com.example.ixithon.model;
+
+/**
+ * Created by abhishek on 8/4/17.
+ */
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class CityDescription {
+  private String id;
+  private String Name;
+  private String countryName;
+  private String description;
+  private String xid;
+  private String keyImageUrl;
+  private String stateName;
+  private String whyToVisit;
+  private Double lat;
+  private Double lng;
+
+  public String getName() {
+    return Name;
+  }
+
+  public void setName(String name) {
+    Name = name;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getCountryName() {
+    return countryName;
+  }
+
+  public void setCountryName(String countryName) {
+    this.countryName = countryName;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getXid() {
+    return xid;
+  }
+
+  public void setXid(String xid) {
+    this.xid = xid;
+  }
+
+  public String getKeyImageUrl() {
+    return keyImageUrl;
+  }
+
+  public void setKeyImageUrl(String keyImageUrl) {
+    this.keyImageUrl = keyImageUrl;
+  }
+
+  public String getStateName() {
+    return stateName;
+  }
+
+  public void setStateName(String stateName) {
+    this.stateName = stateName;
+  }
+
+  public String getWhyToVisit() {
+    return whyToVisit;
+  }
+
+  public void setWhyToVisit(String whyToVisit) {
+    this.whyToVisit = whyToVisit;
+  }
+
+  public Double getLat() {
+    return lat;
+  }
+
+  public void setLat(Double lat) {
+    this.lat = lat;
+  }
+
+  public Double getLng() {
+    return lng;
+  }
+
+  public void setLng(Double lng) {
+    this.lng = lng;
+  }
+
+  public static CityDescription getCityDescriptionFromServer(String JSON){
+    CityDescription cityDescription = new CityDescription();
+
+    try {
+      JSONObject object =  new JSONObject(JSON);
+      JSONObject elementJson =  object.getJSONObject("data");
+      cityDescription.setCountryName(elementJson.getString("countryName"));
+      cityDescription.setDescription(elementJson.getString("description"));
+      cityDescription.setXid(elementJson.getString("xid"));
+      cityDescription.setKeyImageUrl(elementJson.getString("keyImageUrl"));
+      cityDescription.setId(elementJson.getString("id"));
+      cityDescription.setStateName(elementJson.getString("stateName"));
+      cityDescription.setName(elementJson.getString("name"));
+      cityDescription.setLat(elementJson.getDouble("latitude"));
+      cityDescription.setLat(elementJson.getDouble("longitude"));
+      cityDescription.setWhyToVisit(elementJson.getString("whyToVisit"));
+
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    return cityDescription;
+  }
+}
